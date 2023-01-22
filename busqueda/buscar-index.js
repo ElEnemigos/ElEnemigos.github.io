042e79b71b4576adfaceeddc2c7a4f769c933be4
Javier <mario.javiersr@gmail.com>
2023-01-22 15:51:08 +0100

    hide bar

--- busqueda/buscar-index.js
+++ busqueda/buscar-index.js
@@ -90,3 +90,29 @@ document.getElementById("search").addEventListener("keyup", ({key}) => {
        
   }
 })
+
+//const body = document.body;
+const body = document.querySelector(".page-header");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll == 0) {
+        body.classList.remove(scrollUp);
+        return;
+    }
+
+    if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+        // down
+        body.classList.remove(scrollUp);
+        body.classList.add(scrollDown);
+    } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+        // up
+        body.classList.remove(scrollDown);
+        body.classList.add(scrollUp);
+    }
+    lastScroll = currentScroll;
+});
+
