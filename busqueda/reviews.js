91ed653ae0085c85ca106de7f59802bc6f1253ed
ElEnemigos <121864167+ElEnemigos@users.noreply.github.com>
2023-02-08 10:21:51 +0000

    jeje

---
+++ busqueda/reviews.js
@@ -0,0 +1,11 @@
+let reviews = {
+    data:[
+        {
+            gameName:"Ghost of Tsushima",
+            sinopsis:"El juego se remonta al año 1274 en la isla nipona de Tsushima, en plena invasión mongola. Tras fracasar en la defensa y ser uni de los pococs superviviente del ataque enemigo, Jin Sakai deberá dejar de lado su honor como samurái y enfrentarse al ejército mongol para liberar su hogar.",
+        }
+    ]
+}
+
+for(let i of reviews.data){
+}
